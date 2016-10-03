7b33231be229cfda1feaadeb8b48d04a8c190673
Laura Graham <lauragra@microsoft.com>
2016-10-03 11:34:18 -0700

    Syncing updates to deploy.md

--- deploy.cmd
+++ deploy.cmd
@@ -29,6 +29,7 @@ IF NOT DEFINED APIDOCS_PATH (
 
 %APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\content\overview --output %DEPLOYMENT_TARGET_DIR%\overview --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/microsoft-graph-docs/tree/master/content/overview
 
-%APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\content\walkthroughs --output %DEPLOYMENT_TARGET_DIR%\platform --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/microsoft-graph-docs/tree/master/content/walkthroughs
+::remarked below line as we are not going to use walkthroughs folder
+::%APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\content\walkthroughs --output %DEPLOYMENT_TARGET_DIR%\platform --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/microsoft-graph-docs/tree/master/content/walkthroughs
 
-%APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\content\get-started --output %DEPLOYMENT_TARGET_DIR%\platform --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/microsoft-graph-docs/tree/master/content/walkthroughs
+%APIDOCS_PATH%\apidocs.exe publish --path %DEPLOYMENT_SOURCE%\content\get-started --output %DEPLOYMENT_TARGET_DIR%\platform --template %DEPLOYMENT_TEMPLATE% --format mustache --insert-gitInfo true --gitUrl https://github.com/OfficeDev/microsoft-graph-docs/tree/master/content/get-started
