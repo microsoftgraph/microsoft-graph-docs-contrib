d90a08861ac9a3324c7a37064fae0d4aaf8ae670
Millicent Achieng <achieng.milli@gmail.com>
2019-03-06 18:58:30 +0300

    Update PowerShell script to use api-doctor downloaded from GitHub

--- Test-Docs.ps1
+++ Test-Docs.ps1
@@ -5,16 +5,31 @@ Param(
 $repoPath = (Get-Location).Path
 $downloadedApiDoctor = $false
 $downloadedNuGet = $false
+$downloadedCsc = $false
 
 Write-Host "Repository location: ", $repoPath
+Write-Host "GitHub Token: ", $env:GITHUB_TOKEN
+Write-Host "Pull Request: ", $env:SYSTEM_PULLREQUEST_PULLREQUESTNUMBER
+
 
 # Check for ApiDoctor in path
 $apidoc = $null
+
 if (Get-Command "apidoc.exe" -ErrorAction SilentlyContinue) {
     $apidoc = (Get-Command "apidoc.exe").Source
-} else {
-    $nugetPath = $null
-    if (Get-Command "nuget.exe" -ErrorAction SilentlyContinue) {
+} 
+else {
+	# Download apidoctor from GitHub
+	$apidocPath = Join-Path $repoPath -ChildPath "apidoctor"
+	New-Item -ItemType Directory -Force -Path $apidocPath
+	
+	Write-Host "Cloning apidoctor from GitHub"
+	& git clone -b errorLog https://github.com/millicentachieng/apidoctor.git
+	$downloadedApiDoctor = $true
+    	
+	# Get nuget
+	$nugetPath = $null
+	if (Get-Command "nuget.exe" -ErrorAction SilentlyContinue) {
         # Use the existing nuget.exe from the path
         $nugetPath = (Get-Command "nuget.exe").Source
     }
@@ -29,26 +44,37 @@ if (Get-Command "apidoc.exe" -ErrorAction SilentlyContinue) {
         }
         $downloadedNuGet = $true
     }
-
-    $packagesPath = Join-Path $repoPath -ChildPath "apidoctor"
-    $result = New-Item -ItemType Directory -Force -Path $packagesPath
-
-    # install apidoctor from nuget
-    Write-Host "Running nuget.exe from ", $nugetPath
-    $nugetParams = "install", "ApiDoctor", "-OutputDirectory", $packagesPath, "-NonInteractive", "-DisableParallelProcessing"
+	
+	$packagesPath = Join-Path $repoPath -ChildPath "csc"
+    New-Item -ItemType Directory -Force -Path $packagesPath
+	
+	# Download C# compiler from nuget
+	Write-Host "Running nuget.exe from ", $nugetPath
+    $nugetParams = "install", "Microsoft.Net.Compilers", "-OutputDirectory", $packagesPath, "-NonInteractive", "-DisableParallelProcessing"
     & $nugetPath $nugetParams
-
-    if ($LastExitCode -ne 0) { 
+	
+	if ($LastExitCode -ne 0) { 
         # nuget error, so we can't proceed
-        Write-Host "Error installing Api Doctor from NuGet. Aborting."
+        Write-Host "Error installing Microsoft.Net.Compilers from NuGet. Aborting."
         Remove-Item $nugetPath
         exit $LastExitCode
     }
-
-    # get the path to the Api Doctor exe
-    $pkgfolder = Get-ChildItem -LiteralPath $packagesPath -Directory | Where-Object {$_.name -match "ApiDoctor"}
-    $apidoc = [System.IO.Path]::Combine($packagesPath, $pkgfolder.Name, "tools\apidoc.exe")
-    $downloadedApiDoctor = $true
+	
+	# Get path to csc.exe
+	$pkgfolder = Get-ChildItem -LiteralPath $packagesPath -Directory | Where-Object {$_.name -match "Microsoft.Net.Compilers"}
+    $csc = [System.IO.Path]::Combine($packagesPath, $pkgfolder.Name, "tools\csc.exe")
+    $downloadedCsc = $true
+	
+    # Restore all Nuget packages in apidoctor solution
+    $nugetParams = "restore", "$apidocPath\ApiDoctor.sln"
+    & $nugetPath $nugetParams
+    
+	# Compile apidoctor
+	$cscParams = "-define:DEBUG", "-optimize", "-out:apidoc.exe", "-recurse:$apidocPath\*.cs"
+	& $csc $cscParams
+			
+	# Get the path to the apidoctor.exe
+	$apidoc = (Get-Command "apidoc.exe").Source
 }
 
 $lastResultCode = 0
@@ -56,11 +82,13 @@ $lastResultCode = 0
 # run validation at the root of the repository
 $appVeyorUrl = $env:APPVEYOR_API_URL
 
-$parms = "check-all", "--path", $repoPath
+$parms = "check-all", "--path", $repoPath, "--base-branch", "master", "--pull", 1
 if ($appVeyorUrl -ne $null)
 {
     $parms = $parms += "--appveyor-url", $appVeyorUrl
 }
+
+$parms = $parms += "--github-token", $env:GITHUB_TOKEN
 
 & $apidoc $parms
 
@@ -74,6 +102,9 @@ if ($cleanUp -eq $true) {
         Remove-Item $nugetPath 
     }
     if ($downloadedApiDoctor -eq $true) {
+        Remove-Item $apidocPath -Recurse
+    }
+	if ($downloadedCsc -eq $true) {
         Remove-Item $packagesPath -Recurse
     }
 }
